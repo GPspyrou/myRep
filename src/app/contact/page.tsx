51ac8f6b62392b767574514d4c7c6f230defdaae
Giorgos Spyrou <SFnader@gmail.com>
2025-05-11 13:43:15 +0300

    backendupdate2

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from '@/app/lib/ContactForm';
+
+export default function ContactPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-3xl font-bold text-center mb-6">Contact Us</h1>
+      <ContactForm />
+    </main>
+  );
+}
